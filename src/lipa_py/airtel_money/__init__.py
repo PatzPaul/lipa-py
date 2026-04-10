0ba8b0d6cd9dd677a02fd76651c48a77969696e3
patzpaull <patrick.masanja@tz.baridibaridi.com>
2026-04-10 20:38:49 +0300

    fix: export AirtelError from airtel_money package __init__

--- src/lipa_py/airtel_money/__init__.py
+++ src/lipa_py/airtel_money/__init__.py
@@ -1,9 +1,10 @@
-from .client import AirtelClient
+from .client import AirtelClient, AirtelError
 from .schemas import AirtelSTKPushRequest, AirtelSTKPushResponse, AirtelWebhookData
 from .router import airtel_router, set_airtel_webhook_handler
 
 __all__ = [
     "AirtelClient",
+    "AirtelError",
     "AirtelSTKPushRequest",
     "AirtelSTKPushResponse",
     "AirtelWebhookData",
