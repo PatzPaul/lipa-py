69a060bce8b918c2fc198a8ce182e11fa5b3305f
patzpaull <patrick.masanja@tz.baridibaridi.com>
2026-04-10 20:38:48 +0300

    fix: sync __version__ to 0.1.2 to match pyproject.toml

--- src/lipa_py/__init__.py
+++ src/lipa_py/__init__.py
@@ -5,12 +5,84 @@ and Gateways into Python backend applications.
 """
 
 from lipa_py.unified.client import UnifiedPaymentClient
-from lipa_py.unified.schemas import UnifiedPaymentRequest, UnifiedPaymentResponse, UnifiedPaymentConfig
+from lipa_py.unified.schemas import (
+    UnifiedPaymentRequest,
+    UnifiedPaymentResponse,
+    UnifiedPaymentConfig,
+    MpesaConfig,
+    SelcomConfig,
+    SafaricomConfig,
+    TigoConfig,
+    AirtelConfig,
+    TIPSConfig,
+)
 
-__version__ = "0.1.0"
+from lipa_py.mpesa.client import MPesaClient, MpesaError
+from lipa_py.mpesa.schemas import STKPushRequest, MpesaResponse, MpesaWebhookData
+
+from lipa_py.selcom.client import SelcomClient, SelcomError
+from lipa_py.selcom.schemas import SelcomCheckoutRequest, SelcomCheckoutResponse, SelcomWebhookData
+
+from lipa_py.safaricom.client import SafaricomClient, SafaricomError
+from lipa_py.safaricom.schemas import SafaricomSTKPushRequest, SafaricomSTKPushResponse, SafaricomWebhookData
+
+from lipa_py.airtel_money.client import AirtelClient, AirtelError
+from lipa_py.airtel_money.schemas import AirtelSTKPushRequest, AirtelSTKPushResponse, AirtelWebhookData
+
+from lipa_py.tigo_pesa.client import TigoClient, TigoError
+from lipa_py.tigo_pesa.schemas import TigoSTKPushRequest, TigoSTKPushResponse, TigoWebhookData
+
+from lipa_py.tips.client import TIPSClient, TIPSError
+from lipa_py.tips.schemas import TIPSCheckoutRequest, TIPSCheckoutResponse, TIPSWebhookData
+
+__version__ = "0.1.2"
 __all__ = [
+    # Unified
     "UnifiedPaymentClient",
     "UnifiedPaymentConfig",
     "UnifiedPaymentRequest",
-    "UnifiedPaymentResponse"
+    "UnifiedPaymentResponse",
+    # Provider config models
+    "MpesaConfig",
+    "SelcomConfig",
+    "SafaricomConfig",
+    "TigoConfig",
+    "AirtelConfig",
+    "TIPSConfig",
+    # M-Pesa
+    "MPesaClient",
+    "MpesaError",
+    "STKPushRequest",
+    "MpesaResponse",
+    "MpesaWebhookData",
+    # Selcom
+    "SelcomClient",
+    "SelcomError",
+    "SelcomCheckoutRequest",
+    "SelcomCheckoutResponse",
+    "SelcomWebhookData",
+    # Safaricom
+    "SafaricomClient",
+    "SafaricomError",
+    "SafaricomSTKPushRequest",
+    "SafaricomSTKPushResponse",
+    "SafaricomWebhookData",
+    # Airtel Money
+    "AirtelClient",
+    "AirtelError",
+    "AirtelSTKPushRequest",
+    "AirtelSTKPushResponse",
+    "AirtelWebhookData",
+    # Tigo Pesa
+    "TigoClient",
+    "TigoError",
+    "TigoSTKPushRequest",
+    "TigoSTKPushResponse",
+    "TigoWebhookData",
+    # TIPS
+    "TIPSClient",
+    "TIPSError",
+    "TIPSCheckoutRequest",
+    "TIPSCheckoutResponse",
+    "TIPSWebhookData",
 ]
